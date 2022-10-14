a9f4c166bc0e6734f39917bea2ee69f5c0796260
sarawaniolka <waniolka.sara@gmail.com>
2022-10-14 11:22:24 +0200

    intro to neural networks

--- flux03.jl
+++ flux03.jl
@@ -3,21 +3,56 @@
 # imports
 using CSV, DataFrames
 using Flux
+using Flux: onehot, params
+using Plots
 
 # import data
-apples = CSV.read("apples.dat.txt", DataFrame; normalizenames=true)
-bananas = CSV.read("banana.dat.txt", DataFrame; normalizenames=true)
-grapes = CSV.read("Grape_White.dat.txt", DataFrame; normalizenames=true)
-
+apples1 = CSV.read("Apple_Golden_1.dat.txt", DataFrame; normalizenames=true);
+apples2 = CSV.read("Apple_Golden_2.dat.txt", DataFrame; normalizenames=true);
+apples3 = CSV.read("Apple_Golden_3.dat.txt", DataFrame; normalizenames=true);
+bananas = CSV.read("banana.dat.txt", DataFrame; normalizenames=true);
+grapes1 = CSV.read("Grape_White.dat.txt", DataFrame; normalizenames=true);
+grapes2 = CSV.read("Grape_White_2.dat.txt", DataFrame; normalizenames=true);
+apples = vcat(apples1, apples2, apples3)
+grapes = vcat(grapes1, grapes2)
 # rows into vectors
-apples
 x_apples = [ [row.red, row.green] for row in eachrow(apples)]
-x_apples
 x_bananas = [[row.red, row.green] for row in eachrow(bananas)]
 x_grapes = [[row.red, row.green] for row in eachrow(grapes)]
 
 # combine data
-xs = [x_apples, x_bananas, x_grapes]
-xs
+xs = vcat(x_apples, x_bananas, x_grapes)
+
+# manually
+ys = vcat(fill([1,0,0], size(x_apples)), 
+        fill([0,1,0], size(x_bananas)), 
+        fill([0,0,1], size(x_grapes)))
+
+# one-hot vectors
+ys = vcat(fill(onehot(1, 1:3), size(x_apples)),
+          fill(onehot(2, 1:3), size(x_bananas)),
+          fill(onehot(3, 1:3), size(x_grapes)))
 
 
+# model
+model = Dense(2, 3, σ)
+model(xs[end])
+
+L(x,y) = Flux.mse(model(x),y)
+# Opt = SGD(params(model)) does not work
+Opt = Descent()
+data = zip(xs,ys)
+mparams = params(model)
+        for step in 1:100
+	       Flux.train!(L, mparams, data, Opt)
+        end
+
+
+# visualization
+plot()
+contour!(0:0.01:1, 0:0.01:1, (x,y)->model([x,y])[1], levels=[0.5, 0.51], color = cgrad([:blue, :blue]))
+contour!(0:0.01:1, 0:0.01:1, (x,y)->model([x,y])[2], levels=[0.5,0.51], color = cgrad([:green, :green]))
+contour!(0:0.01:1, 0:0.01:1, (x,y)->model([x,y])[3], levels=[0.5,0.51], color = cgrad([:red, :red]))
+scatter!(first.(x_apples), last.(x_apples), m=:cross, label="apples", color = :blue)
+scatter!(first.(x_bananas), last.(x_bananas), m=:circle, label="bananas", color = :green)
+scatter!(first.(x_grapes), last.(x_grapes), m=:square, label="grapes", color = :red)
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ### Learning Flux.jl
 - flux_intro.jl: [[05x08] Intro to Artificial Neural Networks with Flux.jl (1 of 2); Julia Supervised Machine Learning](https://youtu.be/zmlulaxatRs) by doggo dot jl
-- flux01.jl: [Julia Academy: Deep Learning with Flux.jl](https://juliaacademy.com/)
+
+- [Julia Academy: Deep Learning with Flux.jl](https://juliaacademy.com/):
+ - flux01.jl: Building Deep Leraning Models - Intro
+ - flux02.jl: Introduction to Flux
+ - flux03.jl: Introduction to Neural Networks
