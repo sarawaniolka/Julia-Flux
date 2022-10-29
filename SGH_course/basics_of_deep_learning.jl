1a944345f802fdfb02e6b176aefe04c245a15c1e
sarawaniolka <waniolka.sara@gmail.com>
2022-10-29 17:58:30 +0200

    linear model

--- SGH_course/basics_of_deep_learning.jl
+++ SGH_course/basics_of_deep_learning.jl
@@ -1,11 +1,13 @@
 # Classification task on the Australian credit scoring data. 
-# Goal: Distinguisz between the good and bad debtors
+# Goal: Distinguish between the good and bad debtors
 
 using DelimitedFiles
 using PyPlot
 using Statistics
 using StatsBase
 using DataFrames
+
+### LINEAR MODEL
 
 # importing the data
 isfile("australian.dat") ||
@@ -68,5 +70,68 @@ Predict(β, X_train[:,1]) # for a specific observation
 # LEARNING PROCESS
 # binary cross-entropy (log-loss) -> most common function for classification problems
 # we want to find a function that will perfectly seperate our two categories
+L(ŷ, y) = (-y') * log.(ŷ') - (1 .- y') * log.(1 .- ŷ')
 
-  
+# approximating the derivative numeratively is risky bc of the underflow problem
+# (f(x + Δx) - f(x - Δx)/2Δx
+# (0.000000001 - 0.0000000000000211) / 0.00000000000003233233323
+
+
+# gradient symbol - nabla
+
+# To optimize the weights β i use the simple gradient descent method:
+
+# machine epsilon - smallest possible number on the computer
+eps()
+# i use epsilon to represent the Δx
+√eps()
+(f(x + x√eps()) - f(x - x√eps()))/2x√eps()
+
+# step by step:
+β
+LinearIndices(β)
+(LinearIndices(β) .== 2)
+β .+ (LinearIndices(β) .== 2) * β[2] * √eps()
+
+
+function simple_∇(β, X, y)
+    J = L(Predict(β, X),y)[1] # cost function (avg of the loss function), we don't need to divide by the length of the vector, it's a linear transformation
+    ∇ = Float64[] # gradient
+    for i = 1:length(β)
+        b = β[i]
+        β′ = β .+ (LinearIndices(β) .== i) * b * √eps()
+        β′′ = β .- (LinearIndices(β) .== i) * b * √eps()
+        Δf = (L(Predict(β′,X),y)[1] - L(Predict(β′′,X),y)[1]) / (2*b*√eps())
+        push!(∇,Δf)
+    end
+    return J, ∇
+end
+
+
+# optimising the weights β
+# ƞ - learning rate
+# ϵ - stopping criterion
+function solve!(β, X, y; ƞ = 0.001, ϵ= 10^-10, maxit = 50_000)
+    iter = 1
+    Js = Float64[]
+    J, ∇ = simple_∇(β, X, y)
+    push!(Js, J)
+    while true
+        β₀ = deepcopy(β) # copying instead of referencing
+        β .-= ƞ * ∇'
+        J, ∇ = simple_∇(β, X, y)
+        push!(Js, J)
+        stop = maximum(abs.(β .- β₀))
+        stop < ϵ && break
+        iter += 1
+        iter > maxit  && break
+    end
+    return Js
+end
+
+Js = solve!(β, X_train, y_train);
+plot(Js[1:100])
+plt.savefig("mygraph.png")
+
+accuracy(β, X, y, T = 0.5) = sum((Predict(β, X)' .≥ T ).== y)/length(y)
+accuracy(β, X_test, y_test)
